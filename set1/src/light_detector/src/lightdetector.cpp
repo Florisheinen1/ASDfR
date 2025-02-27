44e7ee01201f767fa2671747269307171992e1a3
Floris Heinen <florisheinen1@gmail.com>
2025-02-27 07:47:53 +0100

    Add light printer node

--- set1/src/light_detector/src/lightdetector.cpp
+++ set1/src/light_detector/src/lightdetector.cpp
@@ -6,20 +6,31 @@
 
 class LightDetector : public rclcpp::Node {
 public:
-	LightDetector() : Node("lightdetector") {
+	LightDetector() : Node("lightdetector"), brightness_threshold(50) {
 
 	auto topic_callback = [this](sensor_msgs::msg::Image::SharedPtr image) -> void {
-		RCLCPP_INFO(this->get_logger(), "Received an image with width: '%d' and height: '%d'", image->width, image->height);
 		int brightness = this->calculate_brightness(image);
-		RCLCPP_INFO(this->get_logger(), "Average brightness: %d", brightness);
+		bool lights_on = brightness > this->brightness_threshold;
 
+		RCLCPP_INFO(this->get_logger(), "Lights on: %d (brightness: %d)", lights_on, brightness);
+		
+		auto message = std_msgs::msg::String();
+		message.data = lights_on ? "on" : "off";
+		this->publisher_->publish(message);
+		
 	};
+
+	publisher_ = this->create_publisher<std_msgs::msg::String>("/lights", 10);
+
 	subscription_ = this->create_subscription<sensor_msgs::msg::Image>("/image", 10, topic_callback);
 
 }
 
 private:
 	rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
+	rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+	int brightness_threshold;
+
 
 	int calculate_brightness(sensor_msgs::msg::Image::SharedPtr image) const {
 		int width = image->width;
