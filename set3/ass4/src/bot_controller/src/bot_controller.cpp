5ab61a4dc3678a9bd1c61d9e217300ce8848157d
Floris Heinen <florisheinen1@gmail.com>
2025-04-15 15:49:18 +0200

    Add basic ass4 bot controller

---
+++ set3/ass4/src/bot_controller/src/bot_controller.cpp
@@ -0,0 +1,65 @@
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/point.hpp"
+#include <utility>
+#include <cmath>
+#include <algorithm>
+
+// Around 3 cm/s
+#define SPEED 0.32
+
+class BotController : public rclcpp::Node
+{
+public:
+	BotController() : Node("bot_controller")
+	{
+
+		subscription_ = this->create_subscription<geometry_msgs::msg::Point>(
+			"/trackpos", 10, std::bind(&BotController::point_callback, this, std::placeholders::_1));
+
+		setpoint_pub_ = this->create_publisher<geometry_msgs::msg::Point>("/Ros2Xeno", 10);
+
+		RCLCPP_INFO(this->get_logger(), "bot_controller Node Started (Subscribed to /trackpos)");
+	}
+
+private:
+	rclcpp::Subscription<geometry_msgs::msg::Point>::SharedPtr subscription_;
+	rclcpp::Publisher<geometry_msgs::msg::Point>::SharedPtr setpoint_pub_; // TODO: Change this to Ros2Xeno
+
+	void point_callback(const geometry_msgs::msg::Point::SharedPtr point_msg)
+	{
+		int x_pos = static_cast<int>(point_msg->x);
+		int y_pos = static_cast<int>(point_msg->y);
+
+		if (x_pos == 0 && y_pos == 0)
+		{
+			RCLCPP_WARN(this->get_logger(), "No green object detected. Skipping frame.");
+			return;
+		}
+
+		int corrected_x = std::clamp(x_pos, -150, 150);
+		int corrected_y = std::clamp(y_pos, -100, 100);
+
+		double ratio_x = (double)corrected_x / 460.0; // Arrives at around 3 cm per second, but then in radians per second
+		double ratio_y = (double)corrected_y / 460.0;
+
+		publish_setpoint(ratio_x + 0.1, -ratio_x + 0.1);
+	}
+
+	void publish_setpoint(double left_wheel_speed, double right_wheel_speed)
+	{
+		auto wheel_speeds = geometry_msgs::msg::Point();
+		wheel_speeds.x = left_wheel_speed;
+		wheel_speeds.y = right_wheel_speed;
+		setpoint_pub_->publish(wheel_speeds);
+
+		RCLCPP_INFO(this->get_logger(), "Left: %.2f, Right: %.2f", left_wheel_speed, right_wheel_speed);
+	}
+};
+
+int main(int argc, char *argv[])
+{
+	rclcpp::init(argc, argv);
+	rclcpp::spin(std::make_shared<BotController>());
+	rclcpp::shutdown();
+	return 0;
+}
