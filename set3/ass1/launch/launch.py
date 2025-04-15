f3be1178e5fd7c5b6e67da690f9f403892f83477
Floris Heinen <florisheinen1@gmail.com>
2025-04-15 02:20:12 +0200

    Add basic launch file ass1

---
+++ set3/ass1/launch/launch.py
@@ -0,0 +1,31 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+	return LaunchDescription([
+		# Subs /image and pubs green-masked image /mask
+		Node(
+			package='filterer',
+			executable='masker',
+			name='masker',
+			output='screen'
+		),
+		# Subs /mask and pubs center of gravity on /trackpos
+		Node(
+			package='tracker',
+			executable='tracker',
+			name='tracker',
+			output='screen'
+		),
+		# Subs /trackpos and pubs left and right wheel speed
+		Node(
+			package="asg1_2",
+			executable="bot_controller",
+			name="bot_controller",
+			output="screen"
+		)
+])
