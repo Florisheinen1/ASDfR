1c35eac46853861a2a87f633e28d60892e8b2744
Floris Heinen <florisheinen1@gmail.com>
2025-04-15 12:25:54 +0200

    APply improvements ass2 to ass3

--- set3/ass3/src/group15/include/Group15.hpp
+++ set3/ass3/src/group15/include/Group15.hpp
@@ -4,14 +4,14 @@
 #include "XenoFrt20Sim.hpp"
 #include "LoopController.h"
 
-#pragma pack (1)    //https://carlosvin.github.io/langs/en/posts/cpp-pragma-pack/#_performance_test
+#pragma pack(1) // https://carlosvin.github.io/langs/en/posts/cpp-pragma-pack/#_performance_test
 struct ThisIsAStruct
 {
-    int this_is_a_int = 0;
-    double this_is_a_double = 100.0;
-    float this_is_a_float = 10.0;
-    char this_is_a_char = 'R';
-    bool this_is_a_bool = false;
+	int this_is_a_int = 0;
+	double this_is_a_double = 100.0;
+	float this_is_a_float = 10.0;
+	char this_is_a_char = 'R';
+	bool this_is_a_bool = false;
 };
 
 #pragma pack(0)
@@ -19,28 +19,39 @@ struct ThisIsAStruct
 class Group15 : public XenoFrt20Sim
 {
 public:
-    Group15(uint write_decimator_freq, uint monitor_freq);
-    ~Group15();
+	Group15(uint write_decimator_freq, uint monitor_freq);
+	~Group15();
+
 private:
-    XenoFileHandler file;
-    struct ThisIsAStruct data_to_be_logged;
-    LoopController controller;
+	XenoFileHandler file;
+	struct ThisIsAStruct data_to_be_logged;
+	LoopController controller;
 
-    double u[2];
-    double y[2];
+	int last_left_encoder_value;
+	int last_right_encoder_value;
+	
+	int corrected_left_encoder_value;
+	int corrected_right_encoder_value;
+
+	double u[4];
+	double y[2];
+
+	int get_corrected_encoder_value_difference(int new_value, int old_value) const;
+	double encoder_to_radians(int corrected_encoder_value) const;
+
 protected:
-    //Functions
-    int initialising() override;
-    int initialised() override;
-    int run() override;
-    int stopping() override;
-    int stopped() override;
-    int pausing() override;
-    int paused() override;
-    int error() override;
+	// Functions
+	int initialising() override;
+	int initialised() override;
+	int run() override;
+	int stopping() override;
+	int stopped() override;
+	int pausing() override;
+	int paused() override;
+	int error() override;
 
-    // current error
-    int current_error = 0;
+	// current error
+	int current_error = 0;
 };
 
 #endif // GROUP15
--- set3/ass3/src/group15/src/Group15.cpp
+++ set3/ass3/src/group15/src/Group15.cpp
@@ -1,135 +1,183 @@
 #include "Group15.hpp"
+#include <math.h>
 
-Group15::Group15(uint write_decimator_freq, uint monitor_freq) :
-    XenoFrt20Sim(write_decimator_freq, monitor_freq, file, &data_to_be_logged),
-    file(1,"/home/asdfr-15/logs/log","bin"), controller()
+#define MAX_ENCODER_VALUE 16384
+#define GEAR_RATIO 15.58
+
+Group15::Group15(uint write_decimator_freq, uint monitor_freq) : XenoFrt20Sim(write_decimator_freq, monitor_freq, file, &data_to_be_logged),
+																 file(1, "/home/asdfr-15/logs/log", "bin"), controller()
 {
-     printf("%s: Constructing rampio\n", __FUNCTION__);
-    // Add variables to logger to be logged, has to be done before you can log data
-    logger.addVariable("this_is_a_int", integer);
-    logger.addVariable("this_is_a_double", double_);
-    logger.addVariable("this_is_a_float", float_);
-    logger.addVariable("this_is_a_char", character);
-    logger.addVariable("this_is_a_bool", boolean);
-    
-    // To infinite run the controller, uncomment line below
-    controller.SetFinishTime(0.0);
+	printf("%s: Constructing rampio\n", __FUNCTION__);
+	// Add variables to logger to be logged, has to be done before you can log data
+	logger.addVariable("this_is_a_int", integer);
+	logger.addVariable("this_is_a_double", double_);
+	logger.addVariable("this_is_a_float", float_);
+	logger.addVariable("this_is_a_char", character);
+	logger.addVariable("this_is_a_bool", boolean);
+
+	// To infinite run the controller, uncomment line below
+	controller.SetFinishTime(0.0);
+
+	this->last_left_encoder_value = 0;
+	this->last_right_encoder_value = 0;
+	this->corrected_left_encoder_value = 0;
+	this->corrected_right_encoder_value = 0;
 }
 
 Group15::~Group15()
 {
-    
 }
 
 int Group15::initialising()
 {
-    // Set physical and cyber system up for use in a 
-    // Return 1 to go to initialised state
+	// Set physical and cyber system up for use in a
+	// Return 1 to go to initialised state
 
-    monitor.printf("Hello from initialising\n");      // Do something
+	monitor.printf("Hello from initialising\n"); // Do something
 
-    // The logger has to be initialised at only once
-    logger.initialise();
-    // The FPGA has to be initialised at least once
-    ico_io.init();
+	// The logger has to be initialised at only once
+	logger.initialise();
+	// The FPGA has to be initialised at least once
+	ico_io.init();
 
-    return 1;
+	return 1;
 }
 
 int Group15::initialised()
 {
-    // Keep the physical syste in a state to be used in the run state
-    // Call start() or return 1 to go to run state
+	// Keep the physical syste in a state to be used in the run state
+	// Call start() or return 1 to go to run state
 
-    monitor.printf("Hello from initialised\n");       // Do something
+	monitor.printf("Hello from initialised\n"); // Do something
 
-    return 0;
+	return 0;
 }
 
-/// @brief Converts the read encoder values to a sensible scale
-/// @param encoder_value 
-/// @return The sensible encoder value the plant expects
-int encoder_to_plant(int encoder_value) const {
+int Group15::get_corrected_encoder_value_difference(int new_value, int old_value) const {
+	int half_max_encoder_value = MAX_ENCODER_VALUE / 2;
 
+	int raw_difference = new_value - old_value;
+	int corrected_difference = raw_difference;
+
+	// Correct difference
+	if (raw_difference > half_max_encoder_value) {
+		corrected_difference -= MAX_ENCODER_VALUE;
+	} else if (raw_difference < -half_max_encoder_value) {
+		corrected_difference += MAX_ENCODER_VALUE;
+	}
+
+	return corrected_difference;
+}
+
+double Group15::encoder_to_radians(int corrected_encoder_value) const {
+	auto un_gearbox_encoder = ((double) corrected_encoder_value) / GEAR_RATIO;
+	auto full_rotations = un_gearbox_encoder * 4;
+	auto radians = full_rotations * 2 * M_PI;
+	return radians;
 }
 
 int Group15::run()
 {
-    // Do what you need to do
-    // Return 1 to go to stopping state
+	// Do what you need to do
+	// Return 1 to go to stopping state
 
-    // Start logger
-    logger.start();
+	// Start logger
+	logger.start();
+
+	// Fix encoder wrapping
+	auto right_wheel_encoder = this->sample_data.channel1;
+	auto left_wheel_encoder = this->sample_data.channel2;
+	// Undo wrapping
+	int corrected_left_diff = get_corrected_encoder_value_difference(left_wheel_encoder, this->last_left_encoder_value);
+	int corrected_right_diff = get_corrected_encoder_value_difference(right_wheel_encoder, this->last_right_encoder_value);
+	// Calculate total amount of rotations in radians
+	this->corrected_left_encoder_value += corrected_left_diff;
+	this->corrected_right_encoder_value += corrected_right_diff;
+	// Update old encoder values
+	this->last_left_encoder_value = left_wheel_encoder;
+	this->last_right_encoder_value = right_wheel_encoder;
+
+	// Calculate the angle of the wheels (can be multiple rotations)
+	auto left_wheel_radians = encoder_to_radians(this->corrected_left_encoder_value);
+	// And negate the right wheel for being inverted
+	auto right_wheel_radians = encoder_to_radians(-this->corrected_right_encoder_value);
 
 	auto target_left_wheel_speed = this->ros_data.left_wheel_speed;
 	auto target_right_wheel_speed = this->ros_data.right_wheel_speed;
-    
-	auto right_encoder_value = this->sample_data.channel1;
-	auto left_encoder_value = this->sample_data.channel2;
-	
-	u[0] = left_encoder_value;
-	u[1] = right_encoder_value;
+
+	// Set the PID controller input
+	u[0] = left_wheel_radians;
+	u[1] = right_wheel_radians;
 	u[2] = target_left_wheel_speed;
 	u[3] = target_right_wheel_speed;
 
 	controller.Calculate(u, y);
 
-	auto plant_left_wheel_PWM = (int16_t) y[0];
-	auto plant_right_wheel_PWM = (int16_t) y[1];
+	// Read the output
+	auto controlled_left_speed = u[0];
+	auto controlled_right_speed = u[1];
 
-	// evl_printf("LeftENC: %d, RightENC: %d\n", left_encoder_value, right_encoder_value);
-	evl_printf("LeftPWM: %d, RightPWM: %d\n", plant_left_wheel_PWM, plant_right_wheel_PWM);
-	
-	this->actuate_data.pwm1 = -plant_right_wheel_PWM; // Negative for the inverted motor
-	this->actuate_data.pwm2 = plant_left_wheel_PWM;
+	// And send the motor power
+	actuate_data.pwm1 = controlled_right_speed;
+	actuate_data.pwm2 = controlled_left_speed;
 
-	if(controller.IsFinished())
-        return 1;
+	// For debugging only
+	auto left_power_percentage = (controlled_left_speed / 2048) * 100;
+	auto right_power_percentage = (controlled_right_speed / 2048) * 100;
 	
-    return 0;
+	monitor.printf("Target speed: %f, %f\n", target_left_wheel_speed, target_right_wheel_speed);
+	
+	evl_printf("Encoder left: %d, right: %d\n", this->corrected_left_encoder_value, this->corrected_right_encoder_value);
+	evl_printf("Angle left: %d, right: %d\n", left_wheel_radians, right_wheel_radians);
+	evl_printf("Power left: %d, right: %d\n", left_power_percentage, right_power_percentage);
+	
+	if (controller.IsFinished())
+		return 1;
+
+	return 0;
 }
 
 int Group15::stopping()
 {
-    // Bring the physical system to a stop and set it in a state that the system can be deactivated
-    // Return 1 to go to stopped state
-    logger.stop();                                // Stop logger
-    monitor.printf("Hello from stopping\n");          // Do something
+	// Bring the physical system to a stop and set it in a state that the system can be deactivated
+	// Return 1 to go to stopped state
+	logger.stop();							 // Stop logger
+	monitor.printf("Hello from stopping\n"); // Do something
 
-    return 1;
+	return 1;
 }
 
 int Group15::stopped()
 {
-    // A steady state in which the system can be deactivated whitout harming the physical system
+	// A steady state in which the system can be deactivated whitout harming the physical system
 
-    monitor.printf("Hello from stopping\n");          // Do something
+	monitor.printf("Hello from stopping\n"); // Do something
 
-    return 0;
+	return 0;
 }
 
 int Group15::pausing()
 {
-    // Bring the physical system to a stop as fast as possible without causing harm to the physical system
+	// Bring the physical system to a stop as fast as possible without causing harm to the physical system
 
-    monitor.printf("Hello from pausing\n");           // Do something
-    return 1 ;
+	monitor.printf("Hello from pausing\n"); // Do something
+	return 1;
 }
 
 int Group15::paused()
 {
-    // Keep the physical system in the current physical state
+	// Keep the physical system in the current physical state
 
-    monitor.printf("Hello from paused\n");            // Do something
-    return 0;
+	monitor.printf("Hello from paused\n"); // Do something
+	return 0;
 }
 
 int Group15::error()
 {
-    // Error detected in the system 
-    // Can go to error if the previous state returns 1 from every other state function but initialising 
+	// Error detected in the system
+	// Can go to error if the previous state returns 1 from every other state function but initialising
 
-    monitor.printf("Hello from error\n");             // Do something
+	monitor.printf("Hello from error\n"); // Do something
 
-    return 0;
+	return 0;
 }
